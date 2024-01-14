ba39f06a7b79ed5df0f7a6481aa7fb282f327fac
Mihmall <mihmall@mail.ru>
2024-01-14 17:40:25 +0700

    опять стили

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,16 +6,34 @@ const Home = () => {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/image/b9fv_cb9z_221114.svg" type="image/x-icon" />
+        <link
+          rel="icon"
+          href="/image/b9fv_cb9z_221114.svg"
+          type="image/x-icon"
+        />
         <title>PieceOfCake</title>
       </Head>
-      <main><div className="fon">
-        <h1>Проект интернет-магазина сладостей</h1>
-        
-        
-            <h2>Добро пожаловать на стартовую страницу</h2>
-            <h3></h3>
-            </div>
+      <main>
+        <div className="fon">
+          <h1>Проект интернет-магазина сладостей</h1>
+          <h2>Добро пожаловать на стартовую страницу</h2>
+          <div className="start">
+            <p>
+              Проект интернет-магазина сладостей используется: ReactJS + хуки
+              React Router Axios react-icons concurrently
+            </p>
+            <h3>Быстрый старт:</h3>
+
+            <p>Клонируйте репозиторий c </p>
+            <a href="https://github.com/Mihmall/pieceOfCake.git">
+              https://github.com/Mihmall/pieceOfCake.git
+            </a>
+            <p>
+              Перейдите в папку с проектом Установите зависимости: npm install
+              npm i -D concurrently Запустите проект: npm run dev
+            </p>
+          </div>
+        </div>
       </main>
     </>
   );
